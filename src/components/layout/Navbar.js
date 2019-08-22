1c91696a3a9013c5554e7b137ae88b831a12598c
Gagan Chohan <gaganchohan@icloud.com>
2019-08-22 15:08:55 +1000

    Initial boilerPlate code

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+               <h1>
+                   <i className = 'fab fa-guthub' /> 
+                   </h1> 
+            </div>
+        )
+    }
+}
